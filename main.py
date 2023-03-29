cd15a1809a10d0545b35aa3c08d67d82daee4462
EvanderCorvus <3141514142@protonmail.com>
2023-03-30 01:31:19 +0200

    add source citation

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-key = os.getenv('OPENAI_API_KEY')
+key = os.getenv('OPENAI_API_KEY_PHILIP')
 openai.api_key = key
-model_name = 'gpt-3.5-turbo'
+model_name = 'gpt-4-0314'
 llm = OpenAI(model_name =   model_name,
              temperature=   1)
 
@@ -18,14 +18,14 @@ fname = 'prompt.txt'
 with open(fname,'r') as f:
     template = f.read()
 
-query = 'Tell me in simple words, how the TimeGAN model works.'
+query = """ Explain the holographic principle, to someone who has only machine learning, neural network background and knows only a little bit about quantum theory.""" 
 
 faiss_index = FAISS.load_local('MLLiteratur', OpenAIEmbeddings())
-docs = faiss_index.similarity_search(query, k=5)
+docs = faiss_index.similarity_search(query, k=20)
 literature = ''
-for doc in docs:
-    literature += str(doc.metadata["page"]) + ":" + doc.page_content
 
+for doc in docs:
+    literature += str("source:" + doc.metadata["source"]) + "," + "page:" + str(doc.metadata["page"]) + "content:" + doc.page_content
 
 prompt = lc.PromptTemplate(input_variables=['literature','query'],template=template)
 
