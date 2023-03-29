217fea9a717f97b9f862ccbf857dcf9bd97c4653
EvanderCorvus <3141514142@protonmail.com>
2023-03-29 23:34:20 +0200

    Pdfvectordatabase

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import openai
 import os
 import langchain as lc
 from langchain.llms import OpenAI
-from langchain.document_loaders import UnstructuredPDFLoader
+# from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
 
 key = os.getenv('OPENAI_API_KEY')
 openai.api_key = key
 model_name = 'gpt-3.5-turbo'
 llm = OpenAI(model_name =   model_name,
              temperature=   1)
-loader = UnstructuredPDFLoader('literatur/Nolting, Elektrodynamik',
-                               mode = 'elements')
-literature = loader.load()
+
 
 fname = 'prompt.txt'
 with open(fname,'r') as f:
     template = f.read()
 
-query = 'what are the maxwell equations in integral form?'
+query = 'Tell me in simple words, how the TimeGAN model works.'
+
+faiss_index = FAISS.load_local('MLLiteratur', OpenAIEmbeddings())
+docs = faiss_index.similarity_search(query, k=5)
+literature = ''
+for doc in docs:
+    literature += str(doc.metadata["page"]) + ":" + doc.page_content
+
 
 prompt = lc.PromptTemplate(input_variables=['literature','query'],template=template)
 
